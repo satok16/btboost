a46e7f3a3d9dfbdfa0450e103e0545c1f2510ac8
Satoshi Kataoka <satok@google.com>
2017-08-09 12:40:24 -0700

    fix dep

--- push.sh
+++ push.sh
@@ -3,15 +3,6 @@
 # Error on uninitialized variables..
 set -o nounset
 
-if [[ ! $1 ]]; then
-  echo "Need one input for commit message."
-  exit 1
-fi
-
-echo "commit message = $@"
-
-git commit -a -m "$@"
-
 git push origin master
 
 git show HEAD --name-only| head -n 1
